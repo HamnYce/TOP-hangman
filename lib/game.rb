ef62803b4263f1af1dbef6056b97a4d6e9f0a920
HamnYce <hamadboa@gmail.com>
2022-01-02 22:44:55 +0300

    add win conditions, add end game message

--- lib/game.rb
+++ lib/game.rb
@@ -16,15 +16,23 @@ class Game
 
   def game_loop
     loop do
+      break if @lives_left.zero?
+      break if @correct_indeces.length == @word.length
+
       display_word_state
-      puts 'enter a letter (or \'save\' to save and exit)'
+      puts 'enter a letter (or \'save\')'
       letter = take_user_guess
-      if letter == 'save'
-        File.write('save_file/save.json', to_json)
-        break
-      end
+
+      save_game and break if letter == 'save'
+
       compare_letter_with_word(letter)
     end
+
+    end_game_message
+  end
+
+  def save_game
+    File.write('save_file/save.json', to_json)
   end
 
   def display_word_state
@@ -32,44 +40,58 @@ class Game
     @correct_indeces.each do |i|
       word_state[i] = @word[i]
     end
-    puts word_state
+    puts "\n#{word_state} (wrong guesses: #{@wrong_letters.join(', ')})"
   end
 
   def compare_letter_with_word(letter)
     # test letter against word
-    @word.each_with_index do |chr, i|
-      @correct_indeces.push(i) if chr == letter
+    if @word.include? letter
+      @word.downcase.split('').each_with_index do |chr, i|
+        @correct_indeces.push(i) if chr == letter
+      end
+    else
+      @wrong_letters.push letter
+      @lives_left -= 1
+      puts 'bing bong, that\'s wrong!'
     end
   end
 
   def take_user_guess
     user_guess = ''
-    user_guess = gets.chomp until (user_guess == 'save') ||
-                                  (user_guess.length == 1 &&
-                                  user_guess =~ /[a-z]/i &&
-                                  !@word.include?(user_guess))
+
+    until (user_guess == 'save') || (user_guess.length == 1 &&
+                                    user_guess =~ /[a-z]/i)
+      puts 'enter guess'
+      user_guess = gets.chomp
+    end
+
+    user_guess.downcase
   end
 
-  def to_json(_optional)
-    "{\n
-      \"word\":#{@word},\n
-      \"lives_left\":#{@lives_left},\n
-      \"correct_indeces\":#{@correct_indeces},\n
-      \"wrong_letters\":#{@wrong_letters}\n
+  def end_game_message
+    if @correct_indeces.length == @word.length
+      puts "\n#{@word} (wrong guesses: #{@wrong_letters.join(', ')})"
+      puts 'Winner!'
+    elsif @lives_left.zero?
+      puts 'Oh No! You ran out of lives!'
+    else
+      puts 'Game Saved. Come back later!'
+    end
+    puts "Main Menu\nEnter:\nn - new game\nl - load game\ne - exit game\nh - to see this menu again"
+  end
+
+  def to_json(_optional = nil)
+    "{
+      \"word\":\"#{@word}\",
+      \"lives_left\":#{@lives_left},
+      \"correct_indeces\":#{@correct_indeces},
+      \"wrong_letters\":#{@wrong_letters}
     }"
   end
 
-  def self.from_json(json)
-    save = JSON.parse(json)
-    new(save[:word], save[:lives_left], save[:correct_indeces],
-        save[:wrong_letters])
-  end
-
-  def savefile_name
-    @savefile_name = "#{Dir.glob('save_files/*').max.split('.')[0].succ}.json"
-  end
-
-  def completed?
-    @lives_left.zero? || @correct_indeces.length == @word.length
+  def self.from_json(json_file)
+    save = JSON.parse(json_file.readlines.join)
+    new(save['word'], save['lives_left'], save['correct_indeces'],
+        save['wrong_letters'])
   end
 end
