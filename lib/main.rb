c83f397ddfce9ff5f23d1b4202f090e05e9967a1
HamnYce <hamadboa@gmail.com>
2022-01-02 21:12:07 +0300

    create new_game, load_game function. modularise main menu code

--- lib/main.rb
+++ lib/main.rb
@@ -16,25 +16,39 @@
 #           -> Exit Game -> output_thank_you_message, kill_program
 require_relative 'game'
 
+def new_game
+  words = File.new('512desk.txt', 'r').readlines
+  word = words[rand(words.length)]
+  puts word
+  Game.new(word)
+end
+
+def load_game
+  if File.exist? 'save_file/save.json'
+    File.open('save_file/save.json') { |file| Game.from_json(file) }
+    File.delete('save_file/save.json')
+  else
+    puts 'There is no save file present, try out a new game option instead ^^!'
+  end
+end
+
 choice = ''
 menu = "Main Menu\nEnter:\nn - new game\nl - load game\ne - exit game\nh - to see this menu again"
 puts menu
 
 until choice == 'e'
   choice = gets.chomp
+
   case choice
   when 'n'
-    Game.new
+    new_game
   when 'l'
-    if File.exist? 'save_file/save.json'
-      File.open('save_file/save.json') { |file| Game.from_json(file) }
-      File.delete('save_file/save.json')
-    else
-      puts 'There is no save file present, try out the new game option instead ^^!'
-    end
+    load_game
   when 'h'
     puts menu
+  when 'e'
+    puts 'Thank you for playing :)'
   else
-    puts 'please enter a valid input. Thanks!'
+    puts 'Please enter a valid input. Thanks!'
   end
 end
